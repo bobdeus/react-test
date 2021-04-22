23970ae6ae4325c6c5a2daf47c97b50e4d80cb79
Dustin Dixon <ddixon@claravine.com>
2021-04-22 08:19:24 -0600

    update readme

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://media1.giphy.com/media/BM10yLKooGbEB27GoA/giphy.gif?cid=6104955ebdb349f8dab9a2aa8ec751a7a0f6aff7a3b95826&rid=giphy.gif" className="Fredd" alt="logo" />
+        <h1>
+          WHY GRANT!?!?!
+        </h1>
       </header>
     </div>
   );
