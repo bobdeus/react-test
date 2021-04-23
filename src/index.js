8e19d33e35a7c60c32400fc264fccbc28ce2ed8d
Dustin Dixon <ddixon@claravine.com>
2021-04-23 12:05:19 -0600

    working game

--- src/index.js
+++ src/index.js
@@ -1,17 +1,98 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import calculateWinner from './helpers/gameLogic';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board/>
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Board extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+            winner: null,
+        }
+    }
+
+    renderSquare(i) {
+        return (
+            <Square squareValue={this.state.squares[i]}
+                    onClick={() => this.handleClick(i)}
+            />
+        );
+    }
+
+    handleClick(i) {
+        if(this.state.winner || this.state.squares[i]) return;
+        const squares = this.state.squares.slice();
+        squares[i] = i ? (this.state.xIsNext ? 'X' : 'O') : null;
+
+        const winner = calculateWinner(squares);
+
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+            winner: winner,
+        });
+    }
+
+    render() {
+        const winner = this.state.winner;
+        let status;
+        if(winner) {
+            status = `The Winner is: ${winner}`;
+        } else {
+            status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+        }
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.squareValue}
+        </button>
+    );
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Game/>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
